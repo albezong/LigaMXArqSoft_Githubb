0711b13704b5cec8237958197147bf74c372e47f
Borre <162625904+Borre4023@users.noreply.github.com>
2025-11-21 12:23:46 -0600

    Update 0.0.3 index.js
    Paths de las vistas hasta el momento (beta) por que chonwas me apresura)

--- Frame_LigaMXArqSoft/Vue_LigaMXArqSoft/src/router/index.js
+++ Frame_LigaMXArqSoft/Vue_LigaMXArqSoft/src/router/index.js
@@ -3,6 +3,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/Login/LoginView.vue'
 import RegisterView from '../views/Register/RegisterView.vue'
 import GamesView from '../views/Games/GamesView.vue'
+import TeamsView from '../views/Games/TeamsView.vue'
+import StandingsView from '../views/Games/StandingsView.vue'
+import PlayerView from '../views/Games/PlayerView.vue'
+import TeamsCrud from '@/views/Games/TeamsCrud.vue'
+import PlayersCrud from '@/views/Games/PlayersCrud.vue'
+import UpdateEquipoView from '@/views/Games/UpdateEquipoView.vue'
+
+
 //import JugadoresView from '../views/Jugadores/JugadoresView.vue' // <-- asegúrate que exista
 
 const routes = [
@@ -13,6 +21,13 @@ const routes = [
   { path: '/registro', component: RegisterView },
   { path: '/login', component: LoginView },
   { path: '/games', component: GamesView },
+  { path: '/teams', component: TeamsView },
+  { path: '/standings', component: StandingsView },
+  { path: '/playerscrud', component: PlayersCrud },
+  { path: '/teamscrud', component: TeamsCrud },
+  { path: '/player', component: PlayerView},
+  { path: '/update', component: UpdateEquipoView},
+  
 ]
 
 const router = createRouter({
